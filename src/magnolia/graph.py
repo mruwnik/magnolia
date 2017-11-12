5d517e45f7dcba3f0502b17e2678f755f861d323
Daniel O'Connell <mruwnik@gmail.com>
2017-11-12 22:35:31 +0100

    use the occulsion cones to generate the buds graph

--- src/magnolia/graph.py
+++ src/magnolia/graph.py
@@ -157,44 +157,6 @@ def perendicular_line(b1, b2):
     return lambda b: -1 / m * b.angle2x(b.angle - b1.angle) + b1.height - offset
 
 
-def inner_tangents(b1, b2):
-    """Get the inner tangents between the given buds.
-
-    :param Bud b1: the first bud
-    :param Bud b2: the second bud
-    :rtype: (function, function)
-    returns: line functions representing the tangents
-    """
-    xp = (b1.angle2x(b2.angle - b1.angle) * b1.scale) / ((b1.scale + b2.scale) * b1.radius)
-    yp = (b2.height * b1.scale + b1.height * b2.scale) / (b1.scale + b2.scale)
-
-    worldxp, b, r = b1.angle2x(xp), b1.height, b1.scale
-    try:
-        sqrd = math.sqrt(worldxp**2 + (yp - b)**2 - r**2)
-    except ValueError:
-        sqrd = 0
-    absd = abs(worldxp**2 + (yp - b)**2)
-    x1 = ((r**2) * worldxp + r * (yp - b) * sqrd) / (absd * b1.radius)
-    x2 = ((r**2) * worldxp - r * (yp - b) * sqrd) / (absd * b1.radius)
-
-    y1 = ((r**2) * (yp - b) - r * worldxp * sqrd) / absd + b
-    y2 = ((r**2) * (yp - b) + r * worldxp * sqrd) / absd + b
-
-    def left(b):
-        xdiff = b.angle2x(xp - x1)
-        if xdiff == 0:
-            xdiff = 0.00001
-        return ((yp - y1) * b.angle2x(b.angle - b1.angle - x1)) / xdiff + y1
-
-    def right(b):
-        xdiff = b.angle2x(xp - x2)
-        if xdiff == 0:
-            xdiff = 0.00001
-        return ((yp - y2) * b.angle2x(b.angle - b1.angle - x2)) / xdiff + y2
-
-    return left, right
-
-
 def get_reachable(selected, buds):  # noqa
     """Filter the given buds for all that can be accessed by the selected bud without collisions.
 
@@ -224,28 +186,9 @@ def get_reachable(selected, buds):  # noqa
                 continue
             filtered.append(bud)
     else:
-        # The circles are disjoint - get the inner tangents of the 2 circles - any point that
-        # lies behind the tested bud, but between the 2 tangents can be safely discarded.
-        left, right = inner_tangents(selected, tested)
-
-        # filter out any buds that cannot be accessed without any collisions. The `if` magic below
-        # is because checking if a point lies above or below the `left` and 'right` lines is dependant
-        # on where the tested point lies relative to the selected bud.
-        # WARNING! Any modifications of the following code should be thoroughly tested!
-        for bud in buds[1:]:
-            if left(tested) >= tested.height:
-                if right(tested) >= tested.height:
-                    if left(bud) >= bud.height and right(bud) >= bud.height:
-                        continue
-                elif left(bud) >= bud.height and right(bud) < bud.height:
-                    continue
-            elif left(bud) < bud.height:
-                if right(tested) >= tested.height:
-                    if right(bud) >= bud.height:
-                        continue
-                elif right(bud) < bud.height:
-                    continue
-            filtered.append(bud)
+        # discard all buds that are in the occlusion cone for the selected and tested buds
+        checker = occlusion_cone(selected, tested)
+        filtered = [bud for bud in buds[1:] if not checker(bud)]
     return [tested] + get_reachable(selected, filtered)
 
 
